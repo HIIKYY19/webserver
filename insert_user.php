3185dc5016d33fe74779b685c310a3e98328ef2e
HIIKYY19 <nurrohmanrizky23@gmail.com>
2024-06-27 21:09:20 +0700

    Webserver Retrofit

--- insert_user.php
+++ insert_user.php
@@ -6,15 +6,17 @@ include 'db_config.php';
 $data = json_decode(file_get_contents("php://input"));
 
 // Validate the data
-if (!isset($data->id) || !isset($data->name) || !isset($data->email)) {
+if (!isset($data->id) || !isset($data->name) || !isset($data->email)  || !isset($data->nim) || !isset($data->alamat)) {
     die(json_encode(["error" => "Invalid input"]));
 }
 
 $id = $koneksi->real_escape_string($data->id);
 $name = $koneksi->real_escape_string($data->name);
 $email = $koneksi->real_escape_string($data->email);
+$nim = $koneksi->real_escape_string($data->nim);
+$alamat = $koneksi->real_escape_string($data->alamat);
 
-$sql = "INSERT INTO users (id, name, email) VALUES ('$id','$name', '$email')";
+$sql = "INSERT INTO users (id, name, email, nim, alamat) VALUES ('$id','$name', '$email', '$nim', '$alamat')";
 
 if ($koneksi->query($sql) === TRUE) {
     echo json_encode(["success" => true]);
--- update_user.php
+++ update_user.php
@@ -7,15 +7,17 @@ include 'db_config.php';
 $data = json_decode(file_get_contents("php://input"));
 
 // Validate the data
-if (!isset($data->id) || !isset($data->name) || !isset($data->email)) {
+if (!isset($data->id) || !isset($data->name) || !isset($data->email) || !isset($data->nim) || !isset($data->alamat)) {
     die(json_encode(["error" => "Invalid input"]));
 }
 
 $id = $koneksi->real_escape_string($data->id);
 $name = $koneksi->real_escape_string($data->name);
 $email = $koneksi->real_escape_string($data->email);
+$nim = $koneksi->real_escape_string($data->nim);
+$alamat = $koneksi->real_escape_string($data->alamat);
 
-$sql = "UPDATE users SET name='$name', email='$email' WHERE id=$id";
+$sql = "UPDATE users SET name='$name', email='$email', nim='$nim', alamat='$alamat' WHERE id=$id";
 
 if ($koneksi->query($sql) === TRUE) {
     echo json_encode(["success" => true]);
